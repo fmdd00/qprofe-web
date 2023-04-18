bea9995a5dc0629abab23c6c50dad518850874a4
fmdd00 <francisco63554@hotmail.com>
2023-04-18 00:25:33 -0500

    agregadas rutas a confirmacion de registro y foro

--- src/components/Rutas.js
+++ src/components/Rutas.js
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home'
 import Registro from '../pages/Registro';
 import Login from '../pages/Login'
+import ConfRegistro from '../pages/ConfRegistro';
+import Foro from '../pages/Foro';
 
 function Rutas() {
   return (
@@ -11,6 +13,8 @@ function Rutas() {
             <Route path="/" element={ <Home /> } />
             <Route path="/registro" element={ <Registro /> } />
             <Route path="/login" element={ <Login /> } />
+            <Route path="/confRegistro" element={ <ConfRegistro /> } />
+            <Route path="/foro" element={ <Foro /> } />
         </Routes>
     </section>
   )
