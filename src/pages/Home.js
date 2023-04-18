449eec8280d689bd7471b3ca6ff3204ecc000e4b
fmdd00 <francisco63554@hotmail.com>
2023-04-18 00:08:12 -0500

    componente Home como pagina principal momentanea

---
+++ src/pages/Home.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div className='container'>Bienvenido a QPROFE, tu herramienta para decidir al mejor profesor para tu curso.</div>
+  )
+}
+
+export default Home
