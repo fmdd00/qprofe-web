b7273573a028379a37f7acfd8e35fea3d8fc7280
fmdd00 <francisco63554@hotmail.com>
2023-04-20 23:36:09 -0500

    instalado fontawesome para los iconos

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,18 +3,19 @@ import { Link } from 'react-router-dom';
 function Navbar() {
     return (
         <div>
-            <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <nav className="navbar navbar-expand-lg bg-body-tertiary" style={{ backgroundColor:'lightblue' }}>
                 <div className="container-fluid">
                     <div>
-                        <Link to="/" className='navbar-brand' >QPROFE</Link>
+                        <Link to="/" className='navbar-brand fs-2 fw-bold' >Q'PROFE</Link>
                     </div>
                     <div>
                         <ul className="navbar-nav">
-                            <li className="nav-item">
-                                <Link to="/registro" className='nav-link btn btn-outline-primary'>Registro</Link>
-                            </li>
+                            
                             <li className="nav-item">
                                 <Link to="/login" className='nav-link btn btn-primary' style={{ color:'white' }}>Iniciar Sesión</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/registro" className='nav-link btn btn-outline-dark'>Registro</Link>
                             </li>
                         </ul>
                     </div>
