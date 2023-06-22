5205d5417ba5b560d614e93a877347008db36c10
fmdd00 <francisco63554@hotmail.com>
2023-06-22 04:52:49 -0500

    cambios en el management de las rutas

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,26 @@
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Rutas from './components/Rutas';
+import Home from './pages/Home'
+import Registro from './pages/Registro';
+import Login from './pages/Login'
+import ConfRegistro from './pages/ConfRegistro';
+import Foro from './pages/Foro';
+import Perfil from './pages/Perfil';
 
 function App() {
   return (
-    <Router>
-      <div>
-        <Navbar />
-        <Rutas />
-      </div>
-    </Router>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+        <Route path="/registro" element={<Registro />}></Route>
+        <Route path="/login" element={<Login />}></Route>
+        <Route path="/confRegistro" element={<ConfRegistro />}></Route>
+        <Route path="/foro" element={<Foro />}></Route>
+        <Route path="/perfil" element={<Perfil />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
