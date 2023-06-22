6667e2492cc6907934e7005afb0a38985c96f836
fmdd00 <francisco63554@hotmail.com>
2023-06-22 05:19:12 -0500

    cambios menores

--- client/src/pages/ConfRegistro.js
+++ client/src/pages/ConfRegistro.js
@@ -6,7 +6,11 @@ function ConfRegistro() {
         <div className='container'>
             <div className='mt-4 border border-2 p-4 rounded col-6 mx-auto'>
                 <h1 className='text-center mb-4'>¡Registro Exitoso!</h1>
-                <p>Hola USUARIO. Tu cuenta se ha creado con éxito. Bienvenido a esta comunidad creada por estudiantes para estudiantes. Inicia sesión a continuación con tu nueva cuenta para acceder:</p>
+                <p>
+                    Hola USUARIO. Tu cuenta se ha creado con éxito. Bienvenido a esta
+                    comunidad creada por estudiantes para estudiantes. Inicia sesión
+                    a continuación con tu nueva cuenta para acceder:
+                </p>
                 <div className='d-grid gap-2'>
                     <Link to="/login" className='btn btn-primary'>Iniciar Sesión</Link>
                 </div>
