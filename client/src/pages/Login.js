362134c9b4b9f69bd0626015dbb24c3d097c1e4b
fmdd00 <francisco63554@hotmail.com>
2023-06-22 04:55:07 -0500

    login con validacion en backend

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,93 +1,79 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-import { useRef, useState, useEffect, useContext } from 'react';
-import AuthContext from '../context/AuthProvider';
-
-import axios from '../api/axios';
+import { useState } from 'react';
+import Validation from './LoginValidation';
+import { Link, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 
 function Login() {
-    const { setAuth } = useContext(AuthContext);
-    const userRef = useRef();
-    const errRef = useRef();
+    const [values, setValues] = useState({
+        email: '',
+        password: ''
+    })
 
-    const [user, setUser] = useState('');
-    const [pwd, setPwd] = useState('');
-    const [errMsg, setErrMsg] = useState('');
-    const [success, setSuccess] = useState(false);
+    const [errors, setErrors] = useState({})
 
-    //Este primer useEffect es para que cada vez que el componente es cargado, se enfoque en el input del usuario
-    useEffect(() => {
-        userRef.current.focus();
-    }, [])
+    const navigate = useNavigate();
 
-    //Este segundo eseEffect es para limpiar los errores de mensaje cuando se cambien o modifiquen el usuario y/o contraseña
-    useEffect(() => {
-        setErrMsg('')
-    }, [user, pwd])
+    const handleInput = (event) => {
+        setValues(prev => ({...prev, [event.target.name]: [event.target.value]}))
+    }
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        console.log(user, pwd);
-        setUser('');
-        setPwd('');
-        setSuccess(true);
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        setErrors(Validation(values));
+        if (errors.email === "" && errors.password === "") {
+            axios.post('http://localhost:8081/login', values)
+                .then(res => {
+                    if(res.data === "Success"){
+                        navigate('/foro')
+                    } else {
+                        alert("Los datos ingresados no existen.")
+                    }
+                })
+                .catch(err => console.log(err));
+        }
     }
 
     return (
         <>
-            {success ? (
-                <div className='container'>
-                    <div className='mt-4 border border-2 p-4 rounded col-6 mx-auto'>
-                        <div className='mt-2'>
-                            <h1 className='text-center'>¡Estás Logueado!</h1>
-                        </div>
-                        <div className='mt-3 d-grid gap-2 col-4 mx-auto'>
-                            <Link to="/foro" className="btn btn-primary">Ir al foro</Link>
-                        </div>
+            <div className="container">
+                <div className="mt-4 border border-2 p-4 rounded col-6 mx-auto">
+                    <div className="mt-2">
+                        <h1 className="text-center">INICIAR SESIÓN</h1>
+                    </div>
+                    <div className="mt-4">
+                        <form action='' onSubmit={handleSubmit}>
+                            <div className="mb-3">
+                                <label htmlFor="email" className="form-label">Correo electrónico:</label>
+                                <input
+                                    onChange={handleInput}
+                                    type="email"
+                                    name="email"
+                                    className="form-control" 
+                                    placeholder="Ingresar email"
+                                />
+                                {errors.email && <span className='text-danger'>{errors.email}</span>}
+                                <div className="form-text">Nunca compartiremos tu correo con terceros.</div>
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="password" className="form-label">Contraseña:</label>
+                                <input 
+                                    onChange={handleInput}
+                                    type="password"
+                                    name="password"
+                                    className="form-control" 
+                                    placeholder="Ingresar contraseña" />
+                                    {errors.password && <span className='text-danger'>{errors.password}</span>}
+                            </div>
+                            <div className="d-grid gap-2">
+                                <button type='submit' className='btn btn-primary'>Ingresar</button>
+                                <Link to="/registro" className="btn btn-outline-secondary">Resgístrate</Link>
+                            </div>
+                        </form>
                     </div>
                 </div>
-            ) : (
-                <div className="container">
-                    <p ref={errRef} className={errMsg ? "errMsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
-                    <div className="mt-4 border border-2 p-4 rounded col-6 mx-auto">
-                        <div className="mt-2">
-                            <h1 className="text-center">INICIAR SESIÓN</h1>
-                        </div>
-                        <div className="mt-4">
-                            <form onSubmit={handleSubmit}>
-                                <div className="mb-3">
-                                    <label className="form-label" htmlFor="username">Correo electrónico</label>
-                                    <input
-                                        type="email"
-                                        id="username"
-                                        ref={userRef}
-                                        autoComplete="off"
-                                        onChange={(e) => setUser(e.target.value)}
-                                        value={user}
-                                        required
-                                        className="form-control" placeholder="Ingresar email"
-                                    />
-                                    <div className="form-text">Nunca compartiremos tu correo con terceros.</div>
-                                </div>
-                                <div className="mb-3">
-                                    <label htmlFor="password" className="form-label">Contraseña</label>
-                                    <input type="password"
-                                        id="password"
-                                        onChange={(e) => setPwd(e.target.value)}
-                                        value={pwd}
-                                        required
-                                        className="form-control" placeholder="Ingresar contraseña" />
-                                </div>
-                                <div className="d-grid gap-2 col-4 mx-auto">
-                                    <button>Ingresar</button>
-                                </div>
-                            </form>
-
-                        </div>
-                    </div>
-                </div>
-            )}
+            </div>
         </>
     )
 }
