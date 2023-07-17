6b82ce849cf963f547b74c76807f53bcc0ac3de1
fmdd00 <francisco63554@hotmail.com>
2023-07-17 18:52:29 -0500

    pagina conf

--- client/src/components/ConfRegistro.js
+++ client/src/components/ConfRegistro.js
@@ -7,7 +7,7 @@ function ConfRegistro() {
             <div className='mt-4 border border-2 p-4 rounded col-6 mx-auto'>
                 <h1 className='text-center mb-4'>¡Registro Exitoso!</h1>
                 <p>
-                    Hola USUARIO. Tu cuenta se ha creado con éxito. Bienvenido a esta
+                    Tu cuenta se ha creado con éxito. Bienvenido a esta
                     comunidad creada por estudiantes para estudiantes. Inicia sesión
                     a continuación con tu nueva cuenta para acceder:
                 </p>
